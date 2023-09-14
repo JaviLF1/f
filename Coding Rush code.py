884370b545fc3d5ea2a5116ed691e248cf96b4dc
JaviLF1 <145053537+JaviLF1@users.noreply.github.com>
2023-09-14 12:51:18 -0600

    Create Coding Rush code.py

---
+++ Coding Rush code.py
@@ -0,0 +1,18 @@
+def name():
+  print("Hello, I'm a machine that calculates your exponents :D")
+  print("First write a number for the base and then a number for the          exponent for a result")
+  print("Write the base")
+  a=input()
+  print("Now write the exponent")
+  b=input()
+  result=int(a)**((1)/int(b))
+  print("Your result is", result)
+  print("This was usefull?")
+  c=input()
+  if c == str ("yes"):
+    print("thanks ^^")
+  elif("no"):
+    print("pos te puedes ir a Francia")
+  
+  
+name()
